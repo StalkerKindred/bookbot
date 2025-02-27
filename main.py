b8717b5a226459146e37a23828f59a0bbb9c654f
StalkerKindred <MMMeriaddd@gmail.com>
2025-02-27 15:51:41 +0100

    cache removal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #python3 main.py im lazy af
+from stats import get_book_text
+from stats import get_word_count
+from stats import get_letter_count
 
 book_path = "books/frankenstein.txt"
 
@@ -7,20 +10,5 @@ def main():
     word_count = get_word_count(book_path)
     letter_count = get_letter_count(book_path)
     print(f"{word_count} words found in the document")
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-def get_letter_count(path):
-    string_of_letters = " ".join(get_book_text(path))
-    counter = 0
-    for i in string_of_letters:
-        counter += 1
-    return (counter)
-
-def get_word_count(path):
-    string_of_words = (get_book_text(path)).split()
-    return len(string_of_words)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
+
+book_path = "books/frankenstein.txt"
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 def get_word_count(path):
     string_of_words = (get_book_text(path)).split()
     return len(string_of_words)
+
+def get_letter_count(path):
+    string_of_letters = " ".join(get_book_text(path))
+    counter = 0
+    for i in string_of_letters:
+        counter += 1
+    return (counter)
