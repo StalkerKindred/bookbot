f733c8627bc4e3733a9cb445d164788cf22faa6c
StalkerKindred <MMMeriaddd@gmail.com>
2025-02-28 10:26:01 +0100

    updated main and stats

--- main.py
+++ main.py
@@ -2,13 +2,27 @@
 from stats import get_book_text
 from stats import get_word_count
 from stats import get_letter_count
+from stats import sorting
 
 book_path = "books/frankenstein.txt"
+
+letter_dic = {}
 
 def main():
     text = get_book_text(book_path)
     word_count = get_word_count(book_path)
     letter_count = get_letter_count(book_path)
-    print(f"{word_count} words found in the document")
+    sorted_letter_dic = sorting(letter_dic)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in letter_count:
+        if str.isalpha(i) == True:
+            print (f"{i}:",letter_count[f"{i}"])
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
 
 book_path = "books/frankenstein.txt"
 
+letter_dic = {}
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -11,7 +13,15 @@ def get_word_count(path):
 
 def get_letter_count(path):
     string_of_letters = " ".join(get_book_text(path))
-    counter = 0
+    string_of_letters = str.lower(string_of_letters)
     for i in string_of_letters:
-        counter += 1
-    return (counter)
+        if i in letter_dic:
+            letter_dic[f"{i}"] += 1
+        elif i not in letter_dic:
+            letter_dic[f"{i}"] = 1
+    return letter_dic
+
+def sorting(letter_dic):
+    sorted_letter_dic = sorted(letter_dic.items(), key=lambda x:x[1], reverse=True)
+    back_to_dic = dict(sorted_letter_dic)
+    return back_to_dic
